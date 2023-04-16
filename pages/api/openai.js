cdbc0a0d733aacfdeb513bbae779c5d5f629c7de
sezerano jean chrysostome <sezeranochrisostom123@gmail.com>
2023-04-17 00:44:55 +0200

    docs: adds comments for openapi.js
    Documents openai function inside readme

--- pages/api/openai.js
+++ pages/api/openai.js
@@ -1,16 +1,25 @@
 // filename: openai.js
 // file location: /pages/api/openai.js
+
 import { Configuration, OpenAIApi } from "openai";
 import defaultPrompts from "../../prompts/defaultPrompts";
 
+// Create a configuration object with the OpenAI API key
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+// Create an instance of the OpenAIApi using the configuration
 const openai = new OpenAIApi(configuration);
 
 console.log('API Key:', process.env.OPENAI_API_KEY);
 
+/**
+ * Handle the API request
+ * @param {object} req - The HTTP request object
+ * @param {object} res - The HTTP response object
+ */
 export default async function (req, res) {
+  // Check if the OpenAI API key is configured
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
@@ -21,8 +30,11 @@ export default async function (req, res) {
     return;
   }
 
+  // Extract the payload from the request body
   const payload = req.body.payload || "";
   console.log("The payload is: ", payload);
+
+  // Check if the payload is empty
   if (payload.trim().length === 0) {
     res.status(400).json({
       error: {
@@ -33,26 +45,35 @@ export default async function (req, res) {
   }
 
   try {
+    // Create a user message object
     const userMessage = {
       role: "user",
       content: payload,
     };
 
+    // Combine default prompts and user message into an array of messages
     const messages = [...defaultPrompts, userMessage];
 
+    // Call the OpenAI API to create a chat completion
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: messages,
       temperature: 0,
       max_tokens: 510,
-      "top_p": 0,
+      top_p: 0,
     });
-    res.status(200).json({ result: completion.data.choices[0].message.content });
+
+    // Return the generated content as the result
+    res
+      .status(200)
+      .json({ result: completion.data.choices[0].message.content });
   } catch (error) {
     if (error.response) {
+      // If there's a response error, log and return the error message
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
     } else {
+      // If there's an error with the OpenAI API request, log and return a generic error message
       console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
--- scripts/generateFileStructure.js
+++ scripts/generateFileStructure.js
@@ -38,3 +38,4 @@ function generateContentForFolders() {
 }
 
 generateContentForFolders();
+module.exports = generateContentForFolders
