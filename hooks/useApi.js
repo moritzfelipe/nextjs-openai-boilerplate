f4722bf09400f728b811a017cfea48877e901ec6
Jonath-z <jonathanzihindula95@gmail.com>
2023-09-04 10:36:42 +0200

    refactor: remove the state dependency on useApi hook

--- hooks/useApi.js
+++ hooks/useApi.js
@@ -1,50 +1,45 @@
 // file: /hooks/useApi.js
-import { useState, useEffect } from "react";
+import { useState, useCallback } from "react";
 
-const useApi = (url, method, payload) => {
+const useApi = () => {
   // Define the state variables for data, error, and loading
   const [data, setData] = useState();
   const [error, setError] = useState();
   const [loading, setLoading] = useState(false);
 
-  // Use useEffect to make the API call when the payload changes
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true); // Set loading to true before making the API call
-      try {
-        // Make the API call using the provided url, method, and payload
-        const response = await fetch(url, {
-          method,
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ payload }),
-        });
+  // fetch data to make the API call when the payload changes
+  const fetchData = useCallback(async (url, method, payload) => {
+    setLoading(true); // Set loading to true before making the API call
+    try {
+      // Make the API call using the provided url, method, and payload
+      const response = await fetch(url, {
+        method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ payload }),
+      });
 
-        const result = await response.json();
-        if (response.status !== 200) {
-          // If the response status is not 200, throw an error
-          throw (
-            result.error ||
-            new Error(`Request failed with status ${response.status}`)
-          );
-        }
-
-        setData(result); // Set the data state with the API response
-        setError(null); // Reset the error state to null
-      } catch (error) {
-        setError(error); // Set the error state with the caught error
-      } finally {
-        setLoading(false); // Set loading to false after the API call
+      const result = await response.json();
+      if (response.status !== 200) {
+        // If the response status is not 200, throw an error
+        throw (
+          result.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
-    };
-    if (payload) {
-      fetchData(); // Call fetchData only if payload is provided
+
+      setData(result); // Set the data state with the API response
+      setError(null); // Reset the error state to null
+    } catch (error) {
+      setError(error); // Set the error state with the caught error
+    } finally {
+      setLoading(false); // Set loading to false after the API call
     }
-  }, [url, method, payload]);
+  }, []);
 
   // Return the data, error, and loading states from the hook
-  return { data, error, loading };
+  return { data, error, loading, fetchData };
 };
 
 export default useApi;
